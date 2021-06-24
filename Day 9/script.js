8606427ca430d3bd8f9599132d0584a7f40d2775
Suman <sumandeep4300@gmail.com>
2021-06-24 19:20:24 +0530

    Done with day 9

--- Day 9/script.js
+++ Day 9/script.js
@@ -2,13 +2,12 @@ let seats = document.getElementsByClassName("seat");
 
 for (const seat of seats) {
     seat.addEventListener("click", () => {
-        let booked = seat.classList.toggle("booked");
+        seat.classList.toggle("booked");
 
         const bookedSeats = document.getElementsByClassName("booked").length;
         const remainingSeats = 96 - bookedSeats;
 
-
-    document.querySelector(".bookedSeats").innerHTML = "Booked Seats : " + bookedSeats;
-    const remaining = (document.querySelector(".remainingSeats").innerHTML = "Remaining Seats : " + remainingSeats);
-});
+        document.querySelector(".bookedSeats").innerHTML = "Booked Seats : " + bookedSeats;
+        document.querySelector(".remainingSeats").innerHTML = "Remaining Seats : " + remainingSeats;
+    });
 }  
